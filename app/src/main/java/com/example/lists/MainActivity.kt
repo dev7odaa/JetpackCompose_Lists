576ec50570091e338264ced0211f68756a8871b6
dev7odaa <mah.khalil22@gmail.com>
2022-11-08 17:55:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lists/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.lists
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.lists.ui.theme.ListsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ListContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListContent() {
+    LazyColumn(){
+        items(listOfGyms){
+            GymItem(it)
+        }
+    }
+}
+
+@Composable
+fun GymItem(gym: GymModel){
+    Card(elevation = 4.dp, modifier = Modifier.padding(8.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+            GymIcon(Icons.Filled.Place, Modifier.weight(0.15f))
+            GymDetails(gym, Modifier.weight(0.85f))
+        }
+    }
+}
